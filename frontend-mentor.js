721e6739377a49f53c3cf024d5dd2d94243e7e9b
anointedosara <101458383+anointedosara@users.noreply.github.com>
2022-05-26 02:57:43 +0100

    Add files via upload

---
+++ frontend-mentor.js
@@ -0,0 +1,58 @@
+var i = 0;
+
+function myBody() {
+    if (i >= 0) {
+        document.getElementById("body").style.display = "none"
+        document.getElementById("div6").style.display = "block"
+    }
+}
+
+function myFunction() {
+    if (i >= 0) {
+        document.getElementById("div1").style.background = "rgba(255,255,255,0.3)"
+        document.getElementById("div2").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div3").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div4").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div5").style.background = "rgba(98, 97, 97, 0.3)"
+    }
+}
+
+function myFunctio() {
+    if (i >= 0) {
+        document.getElementById("div1").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div2").style.background = "rgba(255,255,255,0.3)"
+        document.getElementById("div3").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div4").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div5").style.background = "rgba(98, 97, 97, 0.3)"
+    }
+}
+
+function myFuncti() {
+    if (i >= 0) {
+        document.getElementById("div1").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div2").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div3").style.background = "rgba(255,255,255,0.3)"
+        document.getElementById("div4").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div5").style.background = "rgba(98, 97, 97, 0.3)"
+    }
+}
+
+function myFunct() {
+    if (i >= 0) {
+        document.getElementById("div1").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div2").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div3").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div4").style.background = "rgba(255,255,255,0.3)"
+        document.getElementById("div5").style.background = "rgba(98, 97, 97, 0.3)"
+    }
+}
+
+function myFunc() {
+    if (i >= 0) {
+        document.getElementById("div1").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div2").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div3").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div4").style.background = "rgba(98, 97, 97, 0.3)"
+        document.getElementById("div5").style.background = "rgba(255,255,255,0.3)"
+    }
+}
